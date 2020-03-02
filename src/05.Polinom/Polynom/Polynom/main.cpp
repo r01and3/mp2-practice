42f889458e11287f12146e8a40a4e9d2425e21dc
r01and3 <dimka.pronckin@gmail.com>
2020-03-02 23:44:25 +0300

    Final

--- src/05.Polinom/Polynom/Polynom/main.cpp
+++ src/05.Polinom/Polynom/Polynom/main.cpp
@@ -8,14 +8,19 @@ void main() {
 		string str_2 = "3*x^2*y*z^3-3*x^3*y^2*z-4*x^4*y^4*z^4";
 		Polynom polynom_1(str_1);
 		Polynom polynom_2(str_2);
+		cout << "Polynom_1" << endl;
 		cout << polynom_1 << endl;
+		cout << "Polynom_2" << endl;
 		cout << polynom_2 << endl;
 		Polynom polynom_o;
 		polynom_o = polynom_1 + polynom_2;
+		cout << "Polynom_1 + Polynom_2" << endl;
 		cout << polynom_o << endl;
 		polynom_o = polynom_1 - polynom_2;
+		cout << "Polynom_1 - Polynom_2" << endl;
 		cout << polynom_o << endl;
 		polynom_o = polynom_1 * polynom_2;
+		cout << "Polynom_1 * Polynom_2" << endl;
 		cout << polynom_o << endl;
 	}
 	catch (const exception & e) {
